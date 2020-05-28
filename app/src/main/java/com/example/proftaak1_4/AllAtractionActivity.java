fa70a0e26c05a813e9403c76568974f259e48398
Lars <l.rook2002@hotmail.nl>
2020-05-28 09:21:47 +0200

    built a skelet and some other things on the layouts

---
+++ app/src/main/java/com/example/proftaak1_4/AllAtractionActivity.java
@@ -0,0 +1,14 @@
+package com.example.proftaak1_4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AllAtractionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_atraction);
+    }
+}
